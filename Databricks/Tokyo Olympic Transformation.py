aa17e2353a333d186812be4bb530df6640d91304
sreenath569 <70423931+sreenath569@users.noreply.github.com>
2024-04-05 11:42:54 +0530

    Update Tokyo Olympic Transformation.py

--- Databricks/Tokyo Olympic Transformation.py
+++ Databricks/Tokyo Olympic Transformation.py
@@ -8,7 +8,7 @@ configs = {"fs.azure.account.auth.type": "OAuth",
 "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
 "fs.azure.account.oauth2.client.id": "",
 "fs.azure.account.oauth2.client.secret": '',
-"fs.azure.account.oauth2.client.endpoint": "clientendpoint"}
+"fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/tenant_id/oauth2/token"}
 
 
 dbutils.fs.mount(
